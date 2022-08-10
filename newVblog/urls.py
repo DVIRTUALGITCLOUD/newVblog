e1f01e7dd0f403b1e3d502ed84b42163481ca5bd
Debian <bitnami@ip-172-26-1-243.ec2.internal>
2022-08-10 01:40:40 +0000

    first commit

---
+++ newVblog/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+                  path('admin/', admin.site.urls),
+                  path('', include('blog.urls')),
+                  path('users/', include('users.urls')),
+
+                  # Used during the development, not recommended in production
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
